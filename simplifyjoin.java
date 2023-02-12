17f4af6816655302bab443801002b190220e22d7
akashdeep07feb@gmail.com <akashdeep07feb@gmail.com>
2023-02-12 18:06:12 +0530

    changes

--- simplifyjoin.java
+++ simplifyjoin.java
@@ -19,16 +19,19 @@ class simplifyjoin {
     Vector<String[]> arr_alias=new Vector<>();
     while (matcher.find()) {
         String alias=query.substring(matcher.start(),matcher.end());
-        // String[] tem_arr=alias.split(" ");
         System.out.println("  try0\n");
         arr_alias.add(alias.split(" "));
-      //   for (String[] a : arr_alias) {
-      //     System.out.println("  try1\n");
-      //     for(String b:a) {
-      //         System.out.println(b+"  try\n");
-      //     }
-      // }
     }
+
+    final Pattern condition = Pattern.compile("[\\w]+.[\\w]+(\\s*)=(\\s*)[\\w]+.[\\w]+");
+    final Matcher match = condition.matcher(query);
+    Map<String,String> conditionMap = new HashMap<String,String>();
+    while (match.find()){
+      String substr = query.substring(match.start(),match.end());
+      String parts[] = substr.split("=");
+      conditionMap.put(parts[0],parts[1]);
+    }
+    
     return arr_alias;
   }
 }
